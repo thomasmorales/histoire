d91595c10c89977b5bcf64c710b99bc96b902249
Thomas <thomasmorales974@gmail.com>
2025-02-18 14:26:44 +0100

    uncomment auto-open browser

--- histoire.py
+++ histoire.py
@@ -9,13 +9,13 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 
-# def open_browser():
-#     time.sleep(1)
-#     webbrowser.open("http://localhost:8000/src/histoire.html")
+def open_browser():
+    time.sleep(1)
+    webbrowser.open("http://localhost:8000/src/histoire.html")
 
 
-# mon_thread = threading.Thread(target=open_browser)
-# mon_thread.start()
+mon_thread = threading.Thread(target=open_browser)
+mon_thread.start()
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serveur démarré sur le port {PORT}")
